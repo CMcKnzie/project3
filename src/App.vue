5090261fa327f29b058decf4dc7afa7a786f7391
Caitlyn McKenzie <cmckenzi@stu.southtexascollege.edu>
2024-09-15 17:27:06 -0500

    Timer now displays zero, input not functional yet

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,24 @@
 
 import {ref} from "vue"
 
-const varTime = ref('')
-const timer = ref('')
-const countDownSecs = ref('')
+    const seconds = ref(0)
+    const _timerId = null
 
-const startTime =()=>{ 
-  varTime = setInterval(start, 1000);
-  timerDisplay.value = timer.value;
-  countDownSecs = timer.value;
-
-  timerDisplay.value.push({
-  content: timer.value,
-})
-
-} 
-
-const start=() =>{
-  countDownSecs--;
-  timerDisplay.value = countDownSecs;
-  timerDisplay.value
-  if (countDownSecs == -1){
-    stop();
-   timerDisplay.value = "0";
+    const startTimer = () => {
+      seconds.value = timer.value
+      if (seconds.value > 0) {
+        _timerId = setInterval(() => {
+          seconds.value--
+          if (seconds.value <= 0) {
+            clearInterval(_timerId)
+          }
+        }, 1000)
+      }
+    
+    return {
+      seconds,
+    }
   }
-}
-
-const stop = ()=>{
-  clearInterval(varTime)
-}
-
-
-
 </script>
 
 <template>
@@ -44,12 +31,11 @@ const stop = ()=>{
 
         <div class="formItems">
         <input type="number" min = "0" max="60" :key="x" v-model = "timer"> 
-        <button @click="startTime()" type="button">Start Timer </button>
+        <button @click="startTimer(x)" type="button">Start Timer </button>
         </div>
 
         <div class ="Display">
-            <div ref="timerDisplay">
-            </div>
+            <span>{{ seconds }} left </span>
 
         </div>
 		
